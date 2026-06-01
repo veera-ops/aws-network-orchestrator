61243520c1275b8096a1c9460a6ac876b2c4ca02
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-06-01 15:22:47 +0530

    Create main.tf

---
+++ env/main.tf
@@ -0,0 +1,39 @@
+module "dxgw" {
+  source = "../modules/dxgw"
+  environment = var.environment
+  request_id  = var.request_id
+}
+
+module "tgw" {
+  source = "../modules/tgw"
+
+  account_id   = var.account_id
+  account_name = var.account_name
+  region       = var.region
+  environment  = var.environment
+  request_id   = var.request_id
+}
+
+module "vpc" {
+  source = "../modules/vpc"
+
+  account_id   = var.account_id
+  account_name = var.account_name
+  region       = var.region
+  vpc_cidr     = var.vpc_cidr
+  vpc_required = var.vpc_required
+  environment  = var.environment
+  request_id   = var.request_id
+}
+
+module "association" {
+  source = "../modules/association"
+
+  dxgw_id     = module.dxgw.dxgw_id
+  tgw_id      = module.tgw.tgw_id
+  vpc_cidr    = var.vpc_cidr
+  account_id  = var.account_id
+  account_name = var.account_name
+  environment  = var.environment
+  request_id   = var.request_id
+}
