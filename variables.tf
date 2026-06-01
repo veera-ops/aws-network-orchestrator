2d1e35864db35647af79d12f824514e480750c95
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-06-01 16:09:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,11 +4,8 @@ variable "aws_secret_key" {}
 variable "region" {}
 variable "account_id" {}
 variable "account_name" {}
+
 variable "vpc_cidr" {}
 variable "vpc_required" {}
 variable "environment" {}
 variable "request_id" {}
-
-variable "dxgw_id" {
-  default = ""
-}
