d4f43b7f679769d01a501340871e09b739a0d63e
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-06-01 16:02:08 +0530

    Update variables.tf

--- modules/association/variables.tf
+++ modules/association/variables.tf
@@ -1,3 +1,10 @@
 variable "dxgw_id" {}
 variable "tgw_id" {}
 variable "vpc_cidr" {}
+variable "account_id" {}
+variable "account_name" {}
+variable "environment" {}
+variable "request_id" {}
+variable "tags" {
+  type = map(string)
+}
