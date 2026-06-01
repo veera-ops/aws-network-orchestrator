1e6fd1217aae0c62f740f30258d2d9fcd30498af
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-06-01 16:24:27 +0530

    Update main.tf

--- modules/tgw/main.tf
+++ modules/tgw/main.tf
@@ -1,13 +1,7 @@
 resource "aws_ec2_transit_gateway" "score_tgw" {
   description = "TGW-${var.account_name}-${var.region}"
 
-  tags = {
-    Name        = "score-tgw-${var.account_name}-${var.region}"
-    Account     = var.account_id
-    Region      = var.region
-    Environment = var.environment
-    RequestId   = var.request_id
-    CostCenter  = "score-dev"
-    Service     = "tgw"
-  }
+  tags = merge(var.tags, {
+    Service = "tgw"
+  })
 }
