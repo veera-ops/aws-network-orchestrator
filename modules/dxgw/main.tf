9ec3b39dbf648b10e773816d2666555e9c0505cb
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-06-01 16:24:05 +0530

    Update main.tf

--- modules/dxgw/main.tf
+++ modules/dxgw/main.tf
@@ -2,12 +2,7 @@ resource "aws_dx_gateway" "score_dxgw" {
   name            = "score-dxgw-${var.environment}"
   amazon_side_asn = 64512
 
-  tags = {
-    Name        = "score-dxgw-${var.environment}"
-    Environment = var.environment
-    RequestId   = var.request_id
-    Project     = "aws-network-orchestrator"
-    CostCenter  = "score-dev"
-    Service     = "dxgw"
-  }
+  tags = merge(var.tags, {
+    Service = "dxgw"
+  })
 }
