de962e208edf570d89dffd63e838d353107cf546
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-06-01 15:16:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "region" {}
+variable "account_id" {}
+variable "account_name" {}
+variable "vpc_cidr" {}
+variable "vpc_required" {}
+variable "environment" {}
+variable "request_id" {}
+
+variable "dxgw_id" {
+  default = ""
+}
